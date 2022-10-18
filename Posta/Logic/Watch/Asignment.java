6ca02439d564cba5d8132bdd12f7b1d6271e56c5
Acce117 <105556788+Acce117@users.noreply.github.com>
2022-10-18 16:21:09 -0400

    Clase Asignment
    Clase asignment

---
+++ Posta/Logic/Watch/Asignment.java
@@ -0,0 +1,34 @@
+public class Asignment{
+    private Date day;
+
+    private Turn turn;
+
+    private Person personOnWantch;
+
+    public Asignment(Person personOnWatch, Date day, String schedule){
+        this.day = day;
+        setPersonOnWatch(personOnWatch);
+        setTurn(schedule);
+    }
+
+    public void setPersonOnWatch(Person personOnWatch){
+        this.personOnWantch = personOnWantch;
+    }
+
+    private boolean validateSchedule(String schedule){
+        boolean check = true;
+        
+        if(schedule == null || schedule.equals(""))
+            check = false;
+        
+        return check;
+    }
+    public void setTurn(String schedule){
+        if(!validateSchedule)
+            throw new IllegalArgumentException("Horario no válido");
+        
+        this.turn = new Turn(schedule);
+    }
+
+
+}
